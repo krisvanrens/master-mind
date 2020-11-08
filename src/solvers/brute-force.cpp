bf872fe67578f517dc14f10d6f3858cb9a188d8c
Kris van Rens <krisvanrens@gmail.com>
2020-11-08 23:19:26 +0100

    RFT: Brute-force solver.

--- src/solvers/brute-force.cpp
+++ src/solvers/brute-force.cpp
@@ -7,33 +7,38 @@
 #include "../helpers.hpp"
 #include "../master-mind.hpp"
 
+using namespace helpers;
+
 SolverBruteForce::SolverBruteForce(const MasterMind& game)
   : Solver{game} {
 }
 
 unsigned long SolverBruteForce::solve(bool verbose) {
-  Secret guess{};
+  auto generateNextGuess = []() -> std::pair<Secret, unsigned int> {
+    Secret result{};
 
-  const auto guess_scalar_max = static_cast<unsigned int>(std::pow(NUMBER_OF_COLORS, NUMBER_OF_FIELDS));
-  for (unsigned int guess_scalar = 0; guess_scalar < guess_scalar_max; guess_scalar++) {
-    unsigned int remainder = guess_scalar;
-    guess                  = {};
+    static unsigned int guess_scalar = 0;
+    unsigned int        remainder    = guess_scalar++;
 
     for (unsigned int field = NUMBER_OF_FIELDS; (field != 0u) && (remainder != 0); field--) {
       unsigned int exponent    = static_cast<unsigned int>(std::pow(NUMBER_OF_COLORS, field - 1));
       unsigned int coefficient = remainder / exponent;
       remainder -= (coefficient == 0u ? 0u : coefficient * exponent);
-      guess[field - 1] = static_cast<Color>(coefficient);
+      result[field - 1] = static_cast<Color>(coefficient);
     }
+
+    return {result, guess_scalar};
+  };
+
+  while (true) {
+    auto [guess, number_of_tries] = generateNextGuess();
 
     if (verbose) {
-      helpers::print(guess);
+      fmt::print("Trying guess {}..\n", to_string(guess));
     }
 
-    if (helpers::win(game_.guess(guess))) {
-      return guess_scalar + 1;
+    if (win(game_.guess(guess))) {
+      return number_of_tries;
     }
   }
-
-  return 0;
 }
