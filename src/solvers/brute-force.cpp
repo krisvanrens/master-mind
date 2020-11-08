4c4e89e4507fb3548f04b1ad31c9ec5fb8677d10
Kris van Rens <krisvanrens@gmail.com>
2020-11-09 00:41:12 +0100

    CHG: Fix some numeric limit bugs.

--- src/solvers/brute-force.cpp
+++ src/solvers/brute-force.cpp
@@ -14,24 +14,27 @@ SolverBruteForce::SolverBruteForce(const MasterMind& game)
 }
 
 unsigned long SolverBruteForce::solve(bool verbose) {
-  auto generateNextGuess = []() -> std::pair<Secret, unsigned int> {
-    Secret result{};
+  const auto generate_next_guess = []() -> std::pair<Secret, unsigned int> {
+    const auto scalar_to_guess = [](unsigned long scalar) {
+      Secret        result{};
+      unsigned long remainder = scalar;
 
-    static unsigned int guess_scalar = 0;
-    unsigned int        remainder    = guess_scalar++;
+      for (unsigned int field = NUMBER_OF_FIELDS; (field != 0u) && (remainder != 0); field--) {
+        unsigned long exponent    = static_cast<unsigned long>(std::pow(NUMBER_OF_COLORS, field - 1));
+        unsigned long coefficient = remainder / exponent;
+        remainder -= (coefficient == 0ul ? 0ul : coefficient * exponent);
+        result[field - 1] = static_cast<Color>(coefficient);
+      }
 
-    for (unsigned int field = NUMBER_OF_FIELDS; (field != 0u) && (remainder != 0); field--) {
-      unsigned int exponent    = static_cast<unsigned int>(std::pow(NUMBER_OF_COLORS, field - 1));
-      unsigned int coefficient = remainder / exponent;
-      remainder -= (coefficient == 0u ? 0u : coefficient * exponent);
-      result[field - 1] = static_cast<Color>(coefficient);
-    }
+      return result;
+    };
 
-    return {result, guess_scalar};
+    static unsigned long guess_scalar = 0ul;
+    return {scalar_to_guess(guess_scalar++), guess_scalar};
   };
 
   while (true) {
-    auto [guess, number_of_tries] = generateNextGuess();
+    const auto [guess, number_of_tries] = generate_next_guess();
 
     if (verbose) {
       fmt::print("Trying guess #{}: {}..\n", number_of_tries, to_string(guess));
