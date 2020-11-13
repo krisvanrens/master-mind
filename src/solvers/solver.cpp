9e95481d1d96f41d0a2ec5522a11c73d47cf82e2
Kris van Rens <krisvanrens@gmail.com>
2020-11-13 21:30:41 +0100

    FIX: Forgot to increment tries in non-verbose mode.

--- src/solvers/solver.cpp
+++ src/solvers/solver.cpp
@@ -10,8 +10,10 @@ Solver::Solver(const MasterMind& game, bool verbose)
 }
 
 Score Solver::check(const Secret& secret) {
+  number_of_tries_++;
+
   if (verbose_) {
-    fmt::print("Trying secret #{}: [{}]..\n", ++number_of_tries_, helpers::to_string(secret));
+    fmt::print("Trying secret #{}: [{}]..\n", number_of_tries_, helpers::to_string(secret));
   }
 
   return game_.guess(secret);
