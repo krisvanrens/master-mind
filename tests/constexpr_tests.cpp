03cf14687976660dca17f98c378c335741625cb7
Kris van Rens <kris.van.rens@vinotion.nl>
2022-12-28 16:23:41 +0100

    ADD: Tests.

--- tests/constexpr_tests.cpp
+++ tests/constexpr_tests.cpp
@@ -1,12 +1,26 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
 
+#include "../source/helpers.hpp"
+#include "../source/master-mind.hpp"
+
 #ifdef TEST_NONSTATIC_REQUIRE
 #define STATIC_REQUIRE(assertion) REQUIRE(assertion)
 #else
 #define STATIC_REQUIRE(assertion) static_assert(assertion, #assertion)
 #endif
 
-TEST_CASE("XXX") {
-  STATIC_REQUIRE(true);
-}
+TEST_SUITE("MasterMind") {
+  TEST_CASE("Construction") {
+    SUBCASE("Default secret") {
+      constexpr MasterMind mm{Secret{}};
+      STATIC_REQUIRE(helpers::win(mm.guess(Secret{})));
+    }
+
+    SUBCASE("Non-default secret") {
+      constexpr Secret     s = {Color::White};
+      constexpr MasterMind mm{Secret{s}};
+      STATIC_REQUIRE(helpers::win(mm.guess(Secret{s})));
+    }
+  }
+} // TEST_SUITE
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,6 +1,76 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
 
-TEST_CASE("XXX") {
-  REQUIRE(true);
-}
+#include <utility>
+
+#include "../source/helpers.hpp"
+#include "../source/master-mind.hpp"
+
+TEST_SUITE("MasterMind") {
+  TEST_CASE("Construction") {
+    SUBCASE("Default secret") {
+      MasterMind mm{Secret{}};
+      CHECK(helpers::win(mm.guess(Secret{})));
+    }
+
+    SUBCASE("Non-default secret") {
+      constexpr Secret s = {Color::White};
+
+      MasterMind mm{Secret{s}};
+      CHECK(helpers::win(mm.guess(Secret{s})));
+    }
+  }
+
+  TEST_CASE("Copy construction") {
+    constexpr Secret s = {Color::White};
+
+    MasterMind mm1{Secret{s}};
+
+    REQUIRE(helpers::win(mm1.guess(Secret{s})));
+
+    MasterMind mm2{mm1};
+
+    CHECK(helpers::win(mm2.guess(Secret{s})));
+  }
+
+  TEST_CASE("Move construction") {
+    constexpr Secret s = {Color::White};
+
+    MasterMind mm1{Secret{s}};
+
+    REQUIRE(helpers::win(mm1.guess(Secret{s})));
+
+    MasterMind mm2{std::move(mm1)};
+
+    CHECK(helpers::win(mm2.guess(Secret{s})));
+  }
+
+  TEST_CASE("Copy assignment") {
+    constexpr Secret s = {Color::White};
+
+    MasterMind mm1{Secret{s}};
+    MasterMind mm2{Secret{}};
+
+    REQUIRE(helpers::win(mm2.guess(Secret{})));
+    REQUIRE(helpers::win(mm1.guess(Secret{s})));
+
+    mm2 = mm1;
+
+
+    CHECK(helpers::win(mm2.guess(Secret{s})));
+  }
+
+  TEST_CASE("Move assignment") {
+    constexpr Secret s = {Color::White};
+
+    MasterMind mm1{Secret{s}};
+    MasterMind mm2{Secret{}};
+
+    REQUIRE(helpers::win(mm2.guess(Secret{})));
+    REQUIRE(helpers::win(mm1.guess(Secret{s})));
+
+    mm2 = std::move(mm1);
+
+    CHECK(helpers::win(mm2.guess(Secret{s})));
+  }
+} // TEST_SUITE
